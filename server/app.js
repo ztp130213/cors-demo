83a70fee6021d0c5b9811128512506d12eb6398c
zhutianpeng <zhutianpeng@haizhi.com>
2017-06-30 05:54:12 +0800

    fix

--- server/app.js
+++ server/app.js
@@ -5,68 +5,70 @@ var qs = require('querystring');
 var request = require('request');
 var port = 8888;
 
-http.createServer(function(req, res){
+http.createServer(function (req, res) {
     var _url = url.parse(req.url);
     // jsonp
     if (_url.pathname === '/jsonp') {
         var query = _url.query;
         var params = qs.parse(query);
         var f = "";
-    
+
         f = params.callback || 'callback';
-    
-        res.writeHead(200, {"Content-Type": "text/javascript"});
+
+        res.writeHead(200, { "Content-Type": "text/javascript" });
         res.write(f + "({text:'jsonp demo succuss'})");
         res.end();
-    // proxy
+        // proxy
     } else if (_url.pathname === '/proxy') {
         var proxyUrl = "";
-            if (req.url.indexOf('?') > -1) {
+        if (req.url.indexOf('?') > -1) {
             proxyUrl = req.url.substr(req.url.indexOf('?') + 1);
         }
         if (req.method === 'GET') {
             request.get(proxyUrl).pipe(res);
         } else if (req.method === 'POST') {
             var post = '';     //定义了一个post变量，用于暂存请求体的信息
-            req.on('data', function(chunk){    //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+            req.on('data', function (chunk) {    //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
                 post += chunk;
             });
-    
-            req.on('end', function(){    //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+
+            req.on('end', function () {    //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
                 post = qs.parse(post);
                 request({
-                        method: 'POST',
-                        url: proxyUrl,
-                        form: post
-                    }).pipe(res);
+                    method: 'POST',
+                    url: proxyUrl,
+                    form: post
+                }).pipe(res);
             });
         }
-    // 默认首页
+        // 默认首页
     } else if (_url.pathname === '/index') {
-        fs.readFile('./index.html', function(err, data) {
-            res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+        fs.readFile('./index.html', function (err, data) {
+            res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
             res.write(data);
             res.end();
         });
-    // cors
+        // cors
     } else if (_url.pathname === '/cors') {
         var Cookies = {};
-        req.headers.cookie && req.headers.cookie.split(';').forEach(function( Cookie ) {
+        req.headers.cookie && req.headers.cookie.split(';').forEach(function (Cookie) {
             var parts = Cookie.split('=');
-            Cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim();
+            Cookies[parts[0].trim()] = (parts[1] || '').trim();
         });
         if (req.method === 'POST') {
             post = '';
-            req.on('data', function(chunk){ //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+            req.on('data', function (chunk) { //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
                 post += chunk;
             });
-        
-            req.on('end', function(){ //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+
+            req.on('end', function () { //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
                 // post = qs.parse(post);
                 console.log(post);
             });
         }
         if (req.headers.origin) {
+            // 通过http头可以看到cookie和authorization（字段名变成了小写。。前面明明传的是Authorization）
+            console.log(req.headers);
             var header = {
                 "Content-Type": "text/html; charset=UTF-8",
                 // "Access-Control-Allow-Origin": 'http://127.0.0.1:7999,http://localhost:7999',
@@ -79,7 +81,7 @@ http.createServer(function(req, res){
                 header["Access-Control-Allow-Origin"] = req.headers.origin;
             }
             res.writeHead(200, header);
-            
+
             // post才返回，其他请求暂时返回空，这里option要用到
             if (req.method === 'POST') {
                 res.write('cors ' + Cookies['test']);
@@ -89,26 +91,27 @@ http.createServer(function(req, res){
             res.write('hah');
             res.end();
         }
-    // cors with nginx
+        // cors with nginx
     } else if (_url.pathname === '/cors_nginx') {
+        console.log(req.headers);
         var Cookies = {};
-        req.headers.cookie && req.headers.cookie.split(';').forEach(function( Cookie ) {
+        req.headers.cookie && req.headers.cookie.split(';').forEach(function (Cookie) {
             var parts = Cookie.split('=');
-            Cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim();
+            Cookies[parts[0].trim()] = (parts[1] || '').trim();
         });
         if (req.method === 'POST') {
             post = '';
-            req.on('data', function(chunk){ //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+            req.on('data', function (chunk) { //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
                 post += chunk;
             });
-        
-            req.on('end', function(){ //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+
+            req.on('end', function () { //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
                 // post = qs.parse(post);
                 console.log(post);
             });
         }
         if (req.headers.origin) {
-            
+
             var header = {
                 // "Content-Type": "text/html; charset=UTF-8",
                 // "Access-Control-Allow-Origin": 'http://127.0.0.1:7999,http://localhost:7999',
@@ -121,12 +124,12 @@ http.createServer(function(req, res){
             //     header["Access-Control-Allow-Origin"] = req.headers.origin;
             // }
             res.writeHead(200, header);
-            
+
             // post才返回，其他请求暂时返回空，这里option要用到
             if (req.method === 'POST') {
                 res.write('cors ' + Cookies['test']);
             } else {
-                console.log('bbbbbbb');
+                console.log('not post');
             }
             res.end();
         } else {
